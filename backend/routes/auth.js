5a166c3b95f5bf96c59afdd08614ea01d4a85a7b
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2023-08-20 00:22:50 +0530

    Update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
-    obj={
-        a:"this",
-        b:"is",
-        c:"a",
-        d:"test"
+router.post('/',[
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
+], (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj);
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user=>res.json(user)).catch(err=>{console.log(err); res.json({error:"Please enter unique value for email", message: err.message})});
 })
 
 module.exports=router;

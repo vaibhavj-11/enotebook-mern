0f3a66a178caff74e9b4dacd707fb60ce2d86c37
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2023-08-21 22:15:14 +0530

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useContext, useEffect } from 'react';
+import NoteContext from '../context/notes/NoteContext';
 
 const About = () => {
+    const a = useContext(NoteContext);
+    useEffect(() => {
+        a.update();
+        // eslint-disable-next-line
+    }, [])
   return (
     <div>
-      This is About
+      This is About {a.state.name} and he is in class {a.state.class}
     </div>
   )
 }

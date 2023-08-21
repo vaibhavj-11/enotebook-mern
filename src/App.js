185a070bda0af059ee90778be61a4a969d78c6f0
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2023-08-21 21:46:51 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

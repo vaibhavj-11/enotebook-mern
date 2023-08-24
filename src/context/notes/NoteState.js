42fdf63059687b86ba266214349d978a91ac55b8
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2023-08-24 13:49:47 +0530

    Update

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -61,8 +61,51 @@ const NoteState = (props) => {
       ]
       const [notes, setNotes] = useState(notesInital)
     
+      //Add a Note
+        const addNote = (title, description, tag) => {
+            //API Call
+            console.log("Adding a new note");
+            const note = {
+                "_id": "64e620cb30gdca573f9431e8",
+                "user": "64e1a318e5c42d5c9a1ed8dc",
+                "title": title,
+                "description": description,
+                "tag": tag,
+                "date": "2023-08-23T15:07:55.223Z",
+                "__v": 0
+              };
+            setNotes(notes.concat(note));
+        }
+
+        //Delete a Note
+        // const deleteNote = (id) => {
+        //     //API Call
+        //     console.log("Deleting a note");
+        //     const newNotes = notes.filter((note)=>{return note._id !== id});
+        //     setNotes(newNotes);
+        // }
+
+        // //Edit a Note
+        // const editNote = (id, title, description, tag) => {
+        //     //API Call
+        //     console.log("Editing a note");
+        //     let note = {
+        //         "_id": id,
+        //         "user": "64e1a318e5c42d5c9a1ed8dc",
+        //         "title": title,
+        //         "description": description,
+        //         "tag": tag,
+        //         "date": "2023-08-23T15:07:55.223Z",
+        //         "__v": 0
+        //       }
+        //     const newNotes = notes.map((note)=>{
+        //         return note._id === id ? note = note : note;
+        //     })
+        //     setNotes(newNotes);
+        // }
+        
     return (
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote}}>
             {props.children}
         </NoteContext.Provider>
     )

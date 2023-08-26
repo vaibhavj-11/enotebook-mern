1def6ed2bd0cff85d2a575e6ebb89e78a638f7f5
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2023-08-26 23:52:46 +0530

    Update

--- src/components/Login.js
+++ src/components/Login.js
@@ -33,7 +33,8 @@ const Login = (props) => {
     }
 
     return (
-        <div>
+        <div className='mt-3'>
+            <h2 className="my-3">Login to continue to eNotebook</h2>
             <form  onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -32,7 +32,8 @@ const Signup = (props) => {
     }
 
     return (
-        <div className="container">
+        <div className="container mt-3">
+            <h2 className="my-3">Create an account to use eNotebook</h2>
             <form onSubmit={handleSubmit}>
             <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
